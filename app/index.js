61f73e9d54175dedc0429bfcbd4f1a128d6323a9
christophior <chrisvillarreal1018@gmail.com>
2016-03-05 17:18:58 -0600

    added routes and started using children props

--- app/index.js
+++ app/index.js
@@ -1,53 +1,9 @@
-var USER_DATA = {
-    name: 'Chris Villarreal',
-    username: 'christophior',
-    image: 'https://pbs.twimg.com/profile_images/597565924497952768/bBkH8eBe_400x400.jpg'
-}
-
 var React = require('react'),
-    ReactDOM = require('react-dom');
+    ReactDOM = require('react-dom'),
+    routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-    render: function () {
-        return (
-            <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-        )
-    }
-});
-
-var ProfileName = React.createClass({
-    render: function () {
-        return (
-            <div>{this.props.name}</div>
-        )
-    }
-})
-
-var ProfileLink = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <a href={'https://www.github.com/' + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        )
-    }
-})
-
-var Avatar = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username}/>
-            </div>
-        )
-    }
-})
 
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );
