6e879e349267acbf84f17fd0d9b463b6f6f10589
christophior <chrisvillarreal1018@gmail.com>
2016-03-05 15:25:15 -0600

    Nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+var USER_DATA = {
+    name: 'Chris Villarreal',
+    username: 'christophior',
+    image: 'https://pbs.twimg.com/profile_images/597565924497952768/bBkH8eBe_400x400.jpg'
+}
+
 var React = require('react'),
     ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
     render: function () {
         return (
-            <div>Hello Pebbs!</div>
+            <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
         )
     }
 });
 
+var ProfileName = React.createClass({
+    render: function () {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+})
+
+var ProfileLink = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <a href={'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        )
+    }
+})
+
+var Avatar = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username}/>
+            </div>
+        )
+    }
+})
+
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
