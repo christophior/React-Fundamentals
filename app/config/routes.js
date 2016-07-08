d469f8c9c637775e1f91926ac8439e659164ec9d
Chris Villarreal <chrisvillarreal1018@gmail.com>
2016-07-08 13:19:49 -0500

    Added final results route; cmore container and presentation stuff

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,24 +1,26 @@
 var React = require('react'),
-    ReactRouter = require('react-router'),
-    Router = ReactRouter.Router,
-    Route = ReactRouter.Route,
-    IndexRoute = ReactRouter.IndexRoute,
-    hashHistory = ReactRouter.hashHistory;
+	ReactRouter = require('react-router'),
+	Router = ReactRouter.Router,
+	Route = ReactRouter.Route,
+	IndexRoute = ReactRouter.IndexRoute,
+	hashHistory = ReactRouter.hashHistory;
 
 var Main = require('../components/Main'),
-    Home = require('../components/Home'),
-    PromptContainer = require('../containers/PromptContainer'),
-    ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
+	Home = require('../components/Home'),
+	PromptContainer = require('../containers/PromptContainer'),
+	ConfirmBattleContainer = require('../containers/ConfirmBattleContainer'),
+	ResultsContainer = require('../containers/ResultsContainer');
 
 var routes = (
-    <Router history={hashHistory}>
-        <Route path='/' component={Main}>
-            <IndexRoute component={Home} />
-            <Route path='playerOne' header='Player One' component={PromptContainer} />
-            <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
-            <Route path='battle' header='Github Battle' component={ConfirmBattleContainer} />
-        </Route>
-    </Router>
+	<Router history={hashHistory}>
+		<Route path='/' component={Main}>
+			<IndexRoute component={Home} />
+			<Route path='playerOne' header='Player One' component={PromptContainer} />
+			<Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
+			<Route path='battle' header='Github Battle' component={ConfirmBattleContainer} />
+			<Route path='results' component={ResultsContainer} />
+		</Route>
+	</Router>
 );
 
 module.exports = routes;
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -2,12 +2,43 @@ var axios = require('axios');
 
 // optional github API params
 var id = 'YOUR_CLIENT_ID',
-    sec = 'YOUR_SECRET_ID',
-    params = "?client_id=" + id + "&client_secret=" + sec;
+    secret = 'YOUR_SECRET_ID',
+    params = "?client_id=" + id + "&client_secret=" + secret;
 
 function getUserInfo (username) {
     return axios.get('https://api.github.com/users/' + username + params);
 }
+
+function getRepos(username) {
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+}
+
+function getTotalStars(repos) {
+    return repos.data.reduce(function(prev, currentRepo) {
+        return prev + currentRepo.stargazers_count;
+    }, 0);
+}
+
+function getPlayersData(player) {
+    return getRepos(player.login)
+        .then(getTotalStars)
+        .then(function(totalStars) {
+            return {
+                followers: player.followers,
+                totalStars: totalStars
+            };
+        })
+
+}
+
+function calculateScores (players) {
+    return [
+        players[0].followers * 3 + players[0].totalStars,
+        players[1].followers * 3 + players[1].totalStars        
+    ];
+
+}
+
 var helpers = {
     getPlayersInfo: function (players) {
         // fetch some data from github
@@ -20,6 +51,17 @@ var helpers = {
         }).catch(function (err) {
             console.warn('Errors in getPlayersInfo!', err);
         });
+    },
+    battle: function (players) {
+        var playerOneData = getPlayersData(players[0]);
+        var playerTwoData = getPlayersData(players[1]);
+
+        return axios.all([playerOneData, playerTwoData])
+            .then(calculateScores)
+            .catch(function(err) {
+                console.warn('Error in players info: ', err);
+            })
+
     }
 };
 
